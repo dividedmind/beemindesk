16477dbf340eccee39cc091d311900a383ef03b4
Rafał Rzepecki <divided.mind@gmail.com>
2012-12-28 06:41:37 +0100

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Beemindesk::Application.config.secret_token = 'f7a8ac54598bae72cb87aaa2413d9855d36b3c545ff4f6f2d0f94e5f5f5e8a7883be43b967673a39e0c73163553758d8620c020401cb107817dcf8adfaf95601'
